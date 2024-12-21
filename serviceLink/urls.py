bad85433d7b44c1c6459ffa036876bd8b1f43d1e
Hacenaidi <hacenaidi4455@gmail.com>
2024-12-21 16:26:19 +0100

    ajouter la gestion des fichiers statiques dans les urlpatterns et créer un script de construction pour le déploiement

--- serviceLink/urls.py
+++ serviceLink/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('service/', include('service.urls')),
     path('chat/', include('chat.urls')),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
