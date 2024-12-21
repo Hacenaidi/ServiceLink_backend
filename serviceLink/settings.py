292f600d7e1eaa8135d1699fa02489fd9b56eb67
Hacenaidi <hacenaidi4455@gmail.com>
2024-12-21 17:47:32 +0100

    ajouter une origine autorisée pour le frontend déployé et permettre la méthode PATCH dans la configuration CORS

--- serviceLink/settings.py
+++ serviceLink/settings.py
@@ -123,6 +123,7 @@ SECURE_SSL_REDIRECT = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:5173",
+    'https://servicelinkfront.vercel.app'
 
 ]
 
@@ -132,6 +133,7 @@ CORS_ALLOW_METHODS = [
     'PUT',
     'DELETE',
     'OPTIONS',
+    'PATCH'
 ]
 CORS_ALLOW_CREDENTIALS = True
 
