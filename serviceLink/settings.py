5366309eea51daf7d80e69c9b61684f07bf9ba98
Hacenaidi <hacenaidi4455@gmail.com>
2024-12-21 15:43:18 +0100

    ajouter la gestion des fichiers statiques avec WhiteNoise et mettre à jour la configuration de vercel.json

--- serviceLink/settings.py
+++ serviceLink/settings.py
@@ -81,7 +81,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 from datetime import timedelta
 
@@ -195,6 +195,10 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 SESSION_COOKIE_SAMESITE = 'Lax'
